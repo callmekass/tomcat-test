5cad29c1ce455a110cb53379f1565912d69a8f2d
Kassidy <kassidyjo97@gmail.com>
2025-02-01 16:38:06 -0600

    add routes

--- angular-tomcat-test/src/app/app-routing.module.ts
+++ angular-tomcat-test/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './component/home/home.component';
+import { GreetingComponent } from './component/greeting/greeting.component';
+import { DataComponent } from './component/data/data.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'greeting', component: GreetingComponent },
+  { path: 'data', component: DataComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
