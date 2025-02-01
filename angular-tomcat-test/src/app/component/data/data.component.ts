815adc0e42fab0836a6976cdf75e4468899c79ce
Kassidy <kassidyjo97@gmail.com>
2025-02-01 16:38:27 -0600

    add basic components

---
+++ angular-tomcat-test/src/app/component/data/data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.scss'],
+})
+export class DataComponent {}
