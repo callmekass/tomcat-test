6fa96597a29b738c2f21845539fee709a9edaebc
Kassidy <kassidyjo97@gmail.com>
2025-02-01 14:00:03 -0600

    init spring boot 3.4.2 project

---
+++ src/main/java/com/test/tomcat_test/TomcatTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.tomcat_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/tomcat_test/TomcatTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.tomcat_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
