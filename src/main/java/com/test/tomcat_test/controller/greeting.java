0e273f2b72e89c02097be14ef3519a30bc2f59ee
Kassidy <kassidyjo97@gmail.com>
2025-02-01 14:24:37 -0600

    update controller

--- src/main/java/com/test/tomcat_test/controller/greeting.java
+++ src/main/java/com/test/tomcat_test/controller/greeting.java
@@ -1,12 +1,14 @@
 package com.test.tomcat_test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/rest/greeting") 
 public class greeting {
   
-  @GetMapping("/greeting")
+  @GetMapping("/get")
   public String greeting() {
     return "Hello, World!";
   }
